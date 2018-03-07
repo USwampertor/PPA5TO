445e7f262900a4232e195632c85226ecc9c722e9
jalilraziel <idv16c.jzavala@uartesdigitales.edu.mx>
2018-03-07 11:35:14 -0600

    arreglando tus errores

---
+++ Assets/SCRIPTS/UI/Objeto_estatico.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Objeto_estatico : MonoBehaviour {
+
+    public GameObject Inmortal;
+    public Slider Volumen;
+
+    public 
+
+    void Awake()
+    {
+        DontDestroyOnLoad(Inmortal);
+    }
+
+    public void Terminar()
+    {
+        Inmortal.GetComponent<AudioSource>().Pause();
+        Inmortal.GetComponent<AudioSource>().Stop();
+    }
+
+    public void Reiniciar()
+    {
+        Inmortal.GetComponent<AudioSource>().Play();
+    }
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Inmortal.GetComponent<AudioSource>().volume = Volumen.value;
+    }
+}
